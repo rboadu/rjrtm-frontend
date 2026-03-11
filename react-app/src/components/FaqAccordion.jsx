39508739ac7b554d91adbd4535cdfc710c9fdb79
Jaden Ritchie <ritchiejaden801@gmail.com>
2026-03-10 23:34:59 -0400

    added to home page

---
+++ react-app/src/components/FaqAccordion.jsx
@@ -0,0 +1,98 @@
+import React from "react";
+
+const FaqAccordion = () => (
+  <div className="space-y-2">
+    <details className="group [&amp;_summary::-webkit-details-marker]:hidden">
+      <summary className="flex cursor-pointer items-center justify-between gap-4 rounded-lg border border-gray-200 bg-white px-4 py-3 font-medium text-gray-900 hover:bg-gray-50 dark:border-gray-700 dark:bg-gray-900 dark:text-white dark:hover:bg-gray-800">
+        <span>What are the basic features?</span>
+
+        <svg
+          className="w-5 h-5 shrink-0 transition-transform duration-300 group-open:-rotate-180"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M19 9l-7 7-7-7"
+          ></path>
+        </svg>
+      </summary>
+
+      <div className="p-4">
+        <p className="text-gray-700 dark:text-gray-200">
+          Users will be able to create, read, update Countries, States and
+          Cities. On top of this functionality, we give users the opprotunity to
+          play a fund game with the map
+        </p>
+      </div>
+    </details>
+
+    <details className="group [&amp;_summary::-webkit-details-marker]:hidden">
+      <summary className="flex cursor-pointer items-center justify-between gap-4 rounded-lg border border-gray-200 bg-white px-4 py-3 font-medium text-gray-900 hover:bg-gray-50 dark:border-gray-700 dark:bg-gray-900 dark:text-white dark:hover:bg-gray-800">
+        <span>How do I get started?</span>
+
+        <svg
+          className="w-5 h-5 shrink-0 transition-transform duration-300 group-open:-rotate-180"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M19 9l-7 7-7-7"
+          ></path>
+        </svg>
+      </summary>
+
+      <div className="p-4">
+        <p className="text-gray-700 dark:text-gray-200">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+          similique, quae hic dicta quo facere facilis praesentium a sunt, est
+          quia pariatur nam, modi aut minus iste odio consectetur molestias
+          iusto cupiditate ullam laborum veniam quos officia. Quos, temporibus
+          perspiciatis!
+        </p>
+      </div>
+    </details>
+
+    <details className="group [&amp;_summary::-webkit-details-marker]:hidden">
+      <summary className="flex cursor-pointer items-center justify-between gap-4 rounded-lg border border-gray-200 bg-white px-4 py-3 font-medium text-gray-900 hover:bg-gray-50 dark:border-gray-700 dark:bg-gray-900 dark:text-white dark:hover:bg-gray-800">
+        <span>What support options are available?</span>
+
+        <svg
+          className="w-5 h-5 shrink-0 transition-transform duration-300 group-open:-rotate-180"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M19 9l-7 7-7-7"
+          ></path>
+        </svg>
+      </summary>
+
+      <div className="p-4">
+        <p className="text-gray-700 dark:text-gray-200">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+          similique, quae hic dicta quo facere facilis praesentium a sunt, est
+          quia pariatur nam, modi aut minus iste odio consectetur molestias
+          iusto cupiditate ullam laborum veniam quos officia. Quos, temporibus
+          perspiciatis!
+        </p>
+      </div>
+    </details>
+  </div>
+);
+
+export default FaqAccordion;
--- react-app/src/sub_pages/Home.jsx
+++ react-app/src/sub_pages/Home.jsx
@@ -1,9 +1,11 @@
 import { Banner } from "../components/banner";
+import FaqAccordion from "../components/FaqAccordion";
 
 function Home() {
   return (
-    <div className="bg-white dark:bg-gray-900 h-screen flex">
+    <div className="bg-white dark:bg-gray-900 min-h-screen flex flex-col">
       <Banner />
+      <FaqAccordion />
     </div>
   );
 }
